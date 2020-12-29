7c0a2348707a52da0c58d949ca96ad3b92fac7ad
claytonfollett2021 <813551@mystma.org>
2020-12-29 05:20:51 +0000

    1-test-unit-1-test(1)

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#WRITE YOUR CODE IN THIS FILE
+def name():
+    return "Dwight"
+
+print (name())
+
+#def favorites(favColor, favNum):
+#    return My favorite color is "favColor" and my favorite number is "favNum" 
+
+#print ("Blue", "Seven")
+
+def bigAdd(x, y, z, a, b):
+    return ((((x + y) + z) + a) + b)
+
+print (bigAdd(1, 1, 1, 1, 1))
+
+def fourOperations(x, y, z, a, b):
+    return x+y-z*a/b
+
+print (fourOperations(5, 10, 15, 20, 25))
